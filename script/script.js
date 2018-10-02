f7e81d6f083d7aea9f7da46fec64dfcb8890104f
Bestopar <mr.rjcnz2018@mail.ru>
2018-10-02 16:42:53 +0300

    qwe

---
+++ script/script.js
@@ -0,0 +1,37 @@
+/*var o=1;
+    s=1;
+while (o<10) {
+	console.log(o);
+    o++;
+    s=s+o;
+    console.log(s);
+}
+
+*/
+var s=0
+var sum=0
+do{
+s=s+2;
+sum=sum+s;
+}
+while (s<50);
+console.log(sum)
+
+ var kostya = {
+ 	mas:"110kg",
+ 	height:"181sm",
+ 	year:"blue",
+ 	gigant:"true",
+    true:"false"
+ }
+console.log("-_-_-_-_-_-_-_-_-_-")
+ for (var key in kostya) {
+ 	console.log(key);
+ }
+console.log("-_-_-_-_-_-_-_-_-_-")
+for (var key in kostya) {
+ 	console.log(kostya[key]);
+ }
+console.log("-_-_-_-_-_-_-_-_-_-")  
+
+
