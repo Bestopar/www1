b1e4553b85cf9459b02ef590a4a9d27d096ec3e5
Bestopar <mr.rjcnz2018@mail.ru>
2018-10-08 17:45:32 +0300

    kek

---
+++ js/script.js
@@ -0,0 +1,60 @@
+/*var o=1;
+    s=1;
+while (o<10) {
+	console.log(o);
+    o++;
+    s=s+o;
+    console.log(s);
+}
+
+*/
+/*
+var s=0
+var sum=0
+do{
+s=s+2;
+sum=sum+s;
+}
+while (s<50);
+console.log(sum)
+
+ var kostya = {
+ 	mas:"110kg",
+ 	height:"181sm",
+ 	year:"blue",
+ 	gigant:"true",
+    true:"false"
+ }
+console.log("-_-_-_-_-_-_-_-_-_-")
+ for (var key in kostya) {
+ 	console.log(key);
+ }
+console.log("-_-_-_-_-_-_-_-_-_-")
+for (var key in kostya) {
+ 	console.log(kostya[key]);
+ }
+console.log("-_-_-_-_-_-_-_-_-_-")  
+*/
+/*
+var we = function() {
+	var wq=prompt ('Имя')
+	var ww=prompt ('Фамилия')
+	var wr=prompt ('Отчество')
+	console.log(wq)
+	alert(wq+" "+" "+ww+" "+wr)
+	return we;
+}
+var t1=we();
+console.log(t1);
+*/
+/*
+var wt = function() {
+	var wu=prompt ('Имя')
+	var wy=prompt ('Фамилия')
+	if (wu===wy)
+    console.log("одинаковые");
+    else
+    console.log("разные");
+}
+var t1=wt();
+console.log(wt);
